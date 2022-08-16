d3ef2ab0dac845f245cb2a7f88db4203564a2043
annivadas <anni.vadas@gmail.com>
2022-08-16 09:12:57 +0200

    kész

--- js/01/objectsMerge.js
+++ js/01/objectsMerge.js
@@ -1,3 +1,17 @@
-const objectsMerge = () => {};
+/* const objectsMerge = (...objects) => 
+{
+    let result = {}
+    objects.forEach((obj, index) => result[index] = obj)
+    return result
+} */
+
+
+const objectsMerge = (...objects) => {
+    const objectsresult = {...objects};
+    return objectsresult;
+};
 
 export default objectsMerge;
+
+
+
